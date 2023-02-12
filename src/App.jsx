ee6e42d59af28fa5f85f85f7a9fda1d2f1d302a1
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2023-02-12 14:46:53 -0500

    Slightly Fixed UI Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProductsReduce  from './components/ProductsReduce';
 import ProductsFilter  from './components/ProductsFilter'; 
 import ProductsSort from './components/ProductsSort'; 
 import ProductsMap from './components/ProductsMap';
-
+import products from './Data.js'
 function App() {
   const [componentToShow, setComponentToShow] = useState('Reduce');
 
@@ -25,8 +25,12 @@ function App() {
 
   return (
     <div className="App w-[50%] mx-auto"> 
-      <nav className="flex justify-center gap-5">
-        <p> Store Page</p>
+      <nav className="flex justify-center gap-5 py-5">
+        <p> Sample Store Page </p> 
+      </nav> 
+
+      <div className="flex justify-center gap-5 py-5">  
+      
       <button onClick={ProductsReduceHandler} className="relative px-6 py-3 font-bold text-white rounded-lg group">
           <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
           <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-pink-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
@@ -50,9 +54,11 @@ function App() {
           <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-pink-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
           <span className="relative">Products Filter </span> 
         </button>
+      </div>
+      
         
        
-      </nav> 
+      
       
       <section className='flex justify-center py-5'> 
         {componentToShow === 'Filter' && <ProductsFilter/>} 
